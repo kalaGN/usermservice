161e21e1fdea316315a03c7b249f47d6aab7a132
kalaGN <491010750@qq.com>
2022-07-22 21:41:15 +0800

    client and server demo

--- client/client.go
+++ client/client.go
@@ -1,1 +1,52 @@
-package client
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"login"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+//初始化测试数据
+var (
+	dest = flag.String("dest", ":8889", "The server address in the format of host:port")
+	name = flag.String("name", "afei", "The name is login account name")
+	pwd  = flag.String("pwd", "123456", "The passwd is the login account password to login")
+	sign = flag.String("sign", "sign123123123", "The cmd is the login cmd")
+)
+
+func printLogin(client login.LoginServiceClient, req *login.Request) {
+	fmt.Printf("Client: Request sign:%s, name:%s, pwd:%s\n", req.Sign, req.Name, req.Pwd)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	response, err := client.Login(ctx, req)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	fmt.Println("res:" + response)
+}
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial(*dest, grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("failed to Dial")
+	}
+	defer conn.Close()
+
+	client := login.NewLoginServiceClient(conn)
+
+	printLogin(client, &login.Request{
+		Name: *name,
+		Pwd:  *pwd,
+		Sign: *sign,
+	})
+
+}
--- server/v1/service.go
+++ server/v1/service.go
@@ -1,10 +1,46 @@
-/*
- * @Author: afei
- * @Date: 2022-07-22 10:37:36
- * @LastEditors: afei
- * @LastEditTime: 2022-07-22 10:40:45
- * @Description:
- *
- * Copyright (c) 2022 , All Rights Reserved.
- */
-package v1
+package main
+
+import (
+	"context"
+	"fmt"
+	"login"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type LoginService struct {
+}
+
+func (s *LoginService) Login(ctx context.Context, req *login.Request) (*login.Response, error) {
+	fmt.Printf("req name:%s, pwd:%s\n", req.Name, req.Pwd)
+
+	return &login.Response{
+		Code:  "1",
+		Msg:   "login success",
+		Data:  "ok",
+		Token: "ttt",
+	}, nil
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":8889")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("listen on 127.0.0.1:8889")
+
+	grpcServer := grpc.NewServer()
+
+	loginService := LoginService{}
+	login.RegisterLoginServiceServer(grpcServer, &loginService)
+
+	err = grpcServer.Serve(l)
+
+	if err != nil {
+		println(err)
+	}
+
+}
