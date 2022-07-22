c768f422a4ca5eeb9dad22c418df8e665cf60b47
afei <491010750@qq.com>
2022-07-22 11:12:40 +0800

    file struct

---
+++ server/v1/service.go
@@ -0,0 +1,10 @@
+/*
+ * @Author: afei
+ * @Date: 2022-07-22 10:37:36
+ * @LastEditors: afei
+ * @LastEditTime: 2022-07-22 10:40:45
+ * @Description:
+ *
+ * Copyright (c) 2022 , All Rights Reserved.
+ */
+package v1
