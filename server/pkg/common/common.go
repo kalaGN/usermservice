07a2978165b93c58a828621d563985636db5cba5
kalaGN <491010750@qq.com>
2022-08-09 21:43:10 +0800

    common md5sign

--- server/pkg/common/common.go
+++ server/pkg/common/common.go
@@ -1,8 +1,11 @@
 package common
 
 import (
+	"crypto/md5"
 	"encoding/hex"
+	"fmt"
 	"math/rand"
+	"sort"
 	"time"
 )
 
@@ -13,4 +16,30 @@ func GenToken() string {
 	b := make([]byte, uLen)
 	rand.Read(b)
 	return hex.EncodeToString(b)[0:uLen]
+}
+
+//md5 加解密参数
+func CheckSign(params map[string]string, key string, filter string) (sign string) {
+
+	nk := make([]string, 0)
+	for k, _ := range params {
+		if k == filter {
+			continue
+		}
+		nk = append(nk, k)
+	}
+
+	sort.Strings(nk)
+
+	var originStr string
+	for _, v := range nk {
+		originStr += fmt.Sprintf("%v=%v&", v, params[v])
+	}
+
+	originStr += fmt.Sprintf("key=%v", key)
+
+	h := md5.New()
+	h.Write([]byte(originStr))
+	sign = string(h.Sum(nil))
+	return
 }
